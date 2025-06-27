c3fb120aff0ce042d97765a47c9b6b2248e99bc0
Fatemeh <fh815946@gmail.com>
2025-06-27 22:13:19 +0000

    second commit

--- project.sh
+++ project.sh
@@ -3,3 +3,8 @@ read address
 echo "enter the file format"
 read format
 find $address -name "*$format" > backup.conf
+echo "enter the directory address"
+read address2
+timestamp = $(date +"%Y-%m-%d_%H-%M-%S")
+output_file = "${address2}/${timestamp}_backup.tar.gz"
+tar -cvf "$output_file" -T backup.conf
