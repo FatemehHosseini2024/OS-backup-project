b500da5a9f6ec1872713135add37ed4a844e81f7
Fatemeh <fh815946@gmail.com>
2025-07-11 21:49:50 +0000

    change duration time

--- project.sh
+++ project.sh
@@ -7,16 +7,16 @@ echo "enter the directory address"
 read address2
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 output_file="${address2}/${timestamp}_backup.tar.gz"
-start_time=$(date +%s)
+start_time=$(date +%s%N)
 tar -czf "$output_file" -T backup.conf
 status=$?
-end_time=$(date +%s)
+end_time=$(date +%s%N)
 duration=$((end_time - start_time))
 filesize=$(du -m "$output_file" | cut -f1)
 log_file="backup.log"
 now=$(date +"%Y-%m-%d %H:%M:%S")
 if [ $status -eq 0 ]; then
-     echo "[$now] SUCCESS: Backup created at $output_file, Size: ${filesize}MB, Duration: ${duration}s" >> "$log_file"
+     echo "[$now] SUCCESS: Backup created at $output_file, Size: ${filesize}MB, Duration: ${duration}ns" >> "$log_file"
 else 
-     echo "[$now] ERROR: Backup failed, Duration: ${duration}s" >> "$log_file"
+     echo "[$now] ERROR: Backup failed, Duration: ${duration}ns" >> "$log_file"
 fi 
